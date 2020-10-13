09918f9a66396f51acc00a71f41be0d123ed7c52
Michał Dwojak <michal.dwojak92@hotmail.com>
2020-10-13 23:56:13 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fundraising.Startup))]
+namespace Fundraising
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
